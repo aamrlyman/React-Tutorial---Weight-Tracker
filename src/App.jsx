585da0e9510290d3941cbedf59aeedb8b8ec0fda
aamrlyman <aamrlyman@gmail.com>
2022-10-15 15:51:46 -0700

    Add Line Chart useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import React, { useState } from 'react';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
-  const [entries, setEntries] = useState ([{weight: 175, date: "11-23-2021"}, {weight: 176, date: "11-24-2021"}])
+  const [entries, setEntries] = useState ([{weight: 175, date: "10-15-2021"}, {weight: 176, date: "10-16-2021"}])
                                            
   function addNewEntry (entry){
     // ... is like object.assign. Take all the current values of entries and add whatever entry has in it
@@ -16,6 +17,7 @@ function App() {
       {/* adding entries as a prop/peramter to the DisplayEntries function */}
       <DisplayEntries parentEntries={entries} />
       <AddEntryForm addNewEntry={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
        {/*Setting = to function reference not function call(). Reference is like a pointer that we are passing down  */}
     </div>
   );
---
+++ src/Components/EntriesChartTracker/EntriesChartTracker.jsx
@@ -0,0 +1,30 @@
+
+import React, { useState, useEffect } from 'react';
+import { Chart } from "react-google-charts";
+import AddEntryForm from '../AddEntry/AddEntryForm';
+
+
+const EntriesChartTracker = (props) => {
+    
+    const [chartData, setChartData] = useState([])
+
+    useEffect(()=> {
+        let tempChartData = props.parentEntries.map((entry)=> {
+            return [entry.date, entry.weight];
+        })
+        setChartData(tempChartData);
+}, [props.parentEntries]) 
+// useEffect has 2 properties one is an annynomous function and the other is when or how often it should run, default is to run cnostantly
+    return (
+        <Chart
+  chartType="LineChart"
+  data={[["Date", "Weight"], ...chartData]}
+//         X axis   y axis    data points
+  width="100%"
+  height="400px"
+  legendToggle
+/>
+      );
+}
+ 
+export default EntriesChartTracker;
