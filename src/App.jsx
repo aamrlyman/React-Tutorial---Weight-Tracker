ff86ecc25346eba7225ce4b93aa2cb4bc4e36713
aamrlyman <aamrlyman@gmail.com>
2022-10-15 08:59:39 -0700

    add weight, date, and add inputs and buton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import React, { useState } from 'react';
-
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
   const [entries, setEntries] = useState ([{Weight: 175, Date: "11-23-2021"}, 
                                            {Weight: 176, Date: "11-2-2021"}]) 
   return (
     <div className="App">
+      {/* adding entries as a prop/peramter to the DisplayEntries function */}
       <DisplayEntries parentEntries = {entries}/>
+      <AddEntryForm/> 
     </div>
   );
 }
---
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+
+const AddEntryForm = (props) => {
+                // setWeight is a function that we have to call to update the weight value
+    const [weight, setWeight] = useState(0);
+    const [date, setDate] = useState('');
+
+    function handleSubmit (event){
+        event.preventDefault();
+        // The default behavior is to refresh page on submit because the type is submit
+        let newEntry = {
+            // creating an object with the useState weight and date variables
+            weight: {weight},
+            date: {date}
+        }
+        console.log(newEntry);
+    }
+
+    return (
+        // form has an onsubmit attribute that is in this case, calling the handleSubmit function
+        <form onSubmit={handleSubmit}>
+            <label>Weight</label>              
+            {/* onChange is an attribute of useState variables, onChange calls the set weight function based on the event (any change to the weight input) event.target.value takes in the user input on webpage 
+            The value attribute binds the input box to the weight state
+            type= "numer" makes the weight input allow only numbers
+            */}
+            <input type = "Number" value={weight} onChange= {(event) => setWeight(parseFloat(event.target.value))}></input>
+            <label>Date</label>
+            <input type = "date" value={date}  onChange= {(event) => setDate(event.target.value)}></input>
+            <button type = "submit">Add</button>
+
+        </form>
+      );
+}
+ 
+export default AddEntryForm;
