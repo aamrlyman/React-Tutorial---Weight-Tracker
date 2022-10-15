2e02f1358c5c5a532337d465815a8b900beaa4ad
aamrlyman <aamrlyman@gmail.com>
2022-10-15 08:09:28 -0700

    Create display entries componenents instantiated componenet in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import React, { useState } from 'react';
 
 
 function App() {
-  const [entries, setEntries] = useState ([{Weight: 175, Date: "11-23-2021"}]) 
+  const [entries, setEntries] = useState ([{Weight: 175, Date: "11-23-2021"}, 
+                                           {Weight: 176, Date: "11-2-2021"}]) 
   return (
     <div className="App">
-    <table>
-      <thead>
-        <tr>
-          <th>Entry Number</th>
-          <th>Weight</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>175</td>
-          <td>11-23-2021</td>
-        </tr>
-      </tbody>
-    </table>
+      <DisplayEntries parentEntries = {entries}/>
     </div>
   );
 }
